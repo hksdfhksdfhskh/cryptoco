b4140d7af8aefbb281c384c3832fac83c574df6b
Adam Pahlevi <adam.pahlevi@gmail.com>
2019-07-31 23:28:52 +0900

    setup database cleaner for testing env
    so that on each spec test, the db get back to a clean state

--- Gemfile
+++ Gemfile
@@ -18,6 +18,7 @@ gem 'uglifier', '>= 1.3.0'
 gem "typhoeus"
 gem "pry-rails"
 gem "vcr"
+gem 'database_cleaner'
 
 # Use CoffeeScript for .coffee assets and views
 gem 'coffee-rails', '~> 4.2'
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -58,4 +58,15 @@ RSpec.configure do |config|
 
   # FactoryBot configuration
   config.include FactoryBot::Syntax::Methods
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
+
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
+  end
 end
